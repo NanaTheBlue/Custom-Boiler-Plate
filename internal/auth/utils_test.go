759f10ef518dbbd36693bd39d2b10cd8dca42e7d
SkylerBanana <Skyler_Banana@protonmail.com>
2025-07-15 05:11:11 -0400

    Started Work on Service Tests

--- internal/auth/utils_test.go
+++ internal/auth/utils_test.go
@@ -3,6 +3,9 @@ package auth
 import (
 	"testing"
 
+	"github.com/google/uuid"
+
+	"github.com/nanagoboiler/models"
 	"github.com/stretchr/testify/assert"
 	"golang.org/x/crypto/bcrypt"
 )
@@ -29,4 +32,35 @@ func TestHashPassword(t *testing.T) {
 			assert.NoError(t, err, "hashed password should validate against original")
 		}
 	})
+}
+
+func TestValidateJWT(t *testing.T) {
+	type testCase struct {
+		//input params
+		user models.User
+
+		//expected values
+		err error
+	}
+	tests := []testCase{
+		{
+			user: models.User{
+				ID:       uuid.New().String(),
+				Username: "Bingus",
+				Email:    "Bingus@bongus.com",
+			},
+		},
+	}
+	for _, test := range tests {
+		// need to mock
+		token, jti, err := s.generateTokens(&test.user)
+		assert.NoError(t, err)
+		assert.NotEmpty(t, token, jti)
+		user, err := validateJWT(token.Auth_token)
+		assert.NoError(t, err)
+
+		assert.Equal(t, test.user.Username, user.Username, "Token Username doesn't match")
+
+	}
+
 }
